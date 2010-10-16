c2bb2413f0e30dfe7881423e12b32c40328a77ec
Ben Truyman <bentruyman@gmail.com>
2010-10-16 18:11:08 -0500

    Throw some D's on that bitch

--- tips.js
+++ tips.js
@@ -17,8 +17,8 @@ tips.push({
 	message: 'Using <em>All</em> of the XHTML doctypes',
 	example: [
 		'<!DOCTYPE html PUBLIC',
-		'	"-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dt',
-		'	"-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dt',
+		'	"-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd',
+		'	"-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd',
 		'	"-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">'
 	]
 });
