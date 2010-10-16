2c6c859114a688153be972db57f1d0d7be6f3cd7
Ben Truyman <bentruyman@gmail.com>
2010-10-16 15:01:02 -0500

    First commit

---
+++ tips.js
@@ -0,0 +1,26 @@
+var tips = [];
+
+tips.push({
+	author: 'bentruyman',
+	message: 'Using <em>Really</em> Long Explicit Ids',
+	example: [
+		'<div id="EN_US-Introduction-WelcomeMessage-RedText-Container">',
+		'	<p id="EN_US-Introduction-WelcomeMessage-RedText-Container-Paragraph">',
+		'		Welcome to Our Home Page!',
+		'	</p>',
+		'</div>'
+	]
+});
+
+tips.push({
+	author: 'bentruyman',
+	message: 'Using <em>All</em> of the XHTML doctypes',
+	example: [
+		'<!DOCTYPE html PUBLIC',
+		'	"-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dt',
+		'	"-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dt',
+		'	"-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">'
+	]
+});
+
+module.exports = tips;
